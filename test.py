e321817a27761802da1e47ad177acbbd4c205b2b
Tyrin Avery <tavery@demandware.com>
2023-06-04 21:29:29 -0400

    Reverting some of the colors to the old color scheme

--- test.py
+++ test.py
@@ -1,12 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import numpy as np
 import plotly.express as px
 
-
 def main():
-		df = pd.read_csv('/Users/tavery/content-dash/data/teststc1.csv')
-		st.dataframe(df)
-		fig = px.bar(df, title="Monthly Total",x= 'Month',y = 'Views')
-		st.plotly_chart(fig, use_container_width=True)
+	df = pd.read_csv('/Users/tavery/content-dash/data/teststc1.csv')
+	st.dataframe(df)
+	fig = px.bar(df, title="Monthly Total",x= 'Month',y = 'Views')
+	st.plotly_chart(fig, use_container_width=True)
